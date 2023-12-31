b7ed7e8e2204f691b973adf55bbb4eed479d98c1
Mohamed Adel Saleh <mohamed.adel.saleh77@gmail.com>
2023-12-31 22:51:45 +0200

    event details

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -5,7 +5,21 @@ import { handleError } from "../utils";
 import { connectToDatabase } from "../database";
 import User from "../database/models/user.model";
 import Event from "../database/models/event.model";
+import Category from "../database/models/category.model";
 
+const populateEvent = async (query: any) => {
+    return query
+        .populate({
+            path: 'organizer',
+            model: User,
+            select: '_id firstName lastName'
+        })
+        .populate({
+            path: 'category',
+            model: Category,
+            select: '_id name'
+        })
+}
 
 export const createEvent = async ({
     event,
@@ -32,5 +46,18 @@ export const createEvent = async ({
 
     }catch(e){
         handleError(e)
+    }
+}
+
+export const getEventById = async (eventId: string) => {
+    try {
+        await connectToDatabase();
+
+        const event = await populateEvent(Event.findById(eventId));
+        if(!event) throw new Error("Event not found");
+        return JSON.parse(JSON.stringify(event));
+
+    } catch (error) {
+        handleError(error)
     }
 }
